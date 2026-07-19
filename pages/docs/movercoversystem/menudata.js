fb9c6cd477b21d7e8b62b4065dabf7e7aafe194b
Nadeem Wali <61272669+NadzW@users.noreply.github.com>
2026-07-20 00:34:41 +0100

    Update mover cover system docs for 1.1

--- pages/docs/movercoversystem/menudata.js
+++ pages/docs/movercoversystem/menudata.js
@@ -55,6 +55,7 @@ var menudata={children:[
 {text:"h",url:"functions_vars.html#index_h"},
 {text:"l",url:"functions_vars.html#index_l"},
 {text:"m",url:"functions_vars.html#index_m"},
+{text:"n",url:"functions_vars.html#index_n"},
 {text:"r",url:"functions_vars.html#index_r"},
 {text:"s",url:"functions_vars.html#index_s"},
 {text:"t",url:"functions_vars.html#index_t"},
